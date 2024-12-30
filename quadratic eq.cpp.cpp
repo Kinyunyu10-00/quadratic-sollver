0840d4e3f002c1286fe475b52a8abc33271dd0c4
Kinyunyu10-00 <kinyunyubetson@gmail.com>
2024-12-30 23:11:35 +0300

    todays master

---
+++ quadratic eq.cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath> // For sqrt function
+
+using namespace std;
+
+void solveQuadratic(double a, double b, double c) {
+    double discriminant = b*b - 4*a*c;
+    double root1, root2;
+
+ 
