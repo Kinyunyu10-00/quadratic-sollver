13fc89c3f0f02bb2f7c3f2f8e0bc255c6277a463
Kinyunyu10-00 <kinyunyubetson@gmail.com>
2024-12-30 23:20:10 +0300

    sugercane is so sweat

--- quadratic eq.cpp.cpp
+++ quadratic eq.cpp.cpp
@@ -11,3 +11,7 @@ void solveQuadratic(double a, double b, double c) {
     if (discriminant > 0) {
         root1 = (-b + sqrt(discriminant)) / (2*a);
         root2 = (-b - sqrt(discriminant)) / (2*a);
+        
+		cout << "Roots are real and different." << endl;
+        cout << "Root 1: " << root1 << endl;
+        cout << "Root 2: " << root2 << endl;
